af8814fd61ecf197875598c369366342fee10fa1
Lucas Lima <limalucas454@gmail.com>
2020-06-22 16:00:18 -0300

    Link correction

--- footer_template.php
+++ footer_template.php
@@ -1,5 +1,5 @@
 <!-- Start: Footer -->
-<div class="social-icons" style="background-color: rgb(155,21,25);height: 210px;">
+<div class="social-icons" style="background-color: rgb(155,21,25);height: 210px;margin-top: 5%;">
   <a href="https://www.instagram.com/fatima.lima.imoveis/">
     <i class="icon ion-social-instagram-outline" style="color: rgb(255,255,255);"></i>
   </a>
@@ -7,7 +7,7 @@
   <a href="https://www.facebook.com/profile.php?id=100051747426880"><i class="icon ion-social-facebook" style="color: rgb(255,255,255);"></i></a>
     <a
         href="https://api.whatsapp.com/send?phone=5517981093483&text=Ol%C3%A1!%20Eu%20tenho%20interesse%20em%20comprar%20uma%20casa!"><i class="icon ion-social-whatsapp-outline" style="color: rgb(255,255,255);"></i></a>
-<a href="https://articadev.com"><p style="margin-top: 3vh;color: rgb(255,255,255);font-size: 1.25em;">Desenvolvido por Artica</p></a>
+<a href="http://www.articadev.com"><p style="margin-top: 3vh;color: rgb(255,255,255);font-size: 1.25em;">Desenvolvido por Artica</p></a>
 </div>
 <!-- End: Footer -->
 <script src="assets/js/jquery.min.js"></script>
