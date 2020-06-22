8a8328b15f9edc5a7ba93cb7935feb17d798ca86
Lucas Lima <limalucas454@gmail.com>
2020-06-22 02:58:03 -0300

    Added initial carousel and cards

--- header_template.php
+++ header_template.php
@@ -2,44 +2,7 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Fátima Imóveis</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/Footer-Clean.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
-    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
-    <link rel="stylesheet" href="assets/css/Social-Icons.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+
 </head>
 
 <body>
-    <!-- Start: Navbar - XL -->
-    <div>
-        <div class="row">
-            <div class="col" style="display: flex;justify-content: center;"><img src="assets/img/Prancheta%202%20cópia%2014.png" style="margin: 1vh 0;width: 400px;"></div>
-        </div>
-        <div class="row" style="height: 66px;background-color: #9b1519;">
-            <div class="col" style="height: 66px;"></div>
-            <div class="col" style="height: 66px;display: flex;justify-content: center;align-items: center;">
-                <a href="#">
-                    <h3 style="color: rgb(255,255,255);font-family: Roboto;margin-bottom: 0px;">Home</h3>
-                </a>
-                <a href="#" style="margin: 0px 20px;">
-                    <h3 style="color: rgb(255,255,255);font-family: Roboto;margin-bottom: 0px;">Imóveis</h3>
-                </a>
-                <a href="#">
-                    <h3 style="color: rgb(255,255,255);font-family: Roboto;margin-bottom: 0px;">Contato</h3>
-                </a>
-            </div>
-            <div class="col" style="height: 66px;display: flex;align-items: center;justify-content: flex-end;">
-                <a href="#" style="margin: 0px 20px;">
-                    <h3 style="color: rgb(255,255,255);font-family: Roboto;margin-bottom: 0px;margin-right: 0px;margin-left: 0px;">Pesquisa<i class="fa fa-arrow-down" style="color: rgb(255,255,255);margin-right: 0px;margin-left: 10px;font-size: 20px;"></i></h3>
-                </a>
-            </div>
-        </div>
-    </div>
-    <!-- End: Navbar - XL -->
