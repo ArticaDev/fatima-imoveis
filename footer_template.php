8049a6bbd7e81937b190681386f898342024dc3f
Lucas Lima <limalucas454@gmail.com>
2020-06-22 02:47:32 -0300

    Added footer with social media icons

--- footer_template.php
+++ footer_template.php
@@ -1,5 +1,14 @@
-
-
+<!-- Start: Footer -->
+<div class="social-icons" style="background-color: rgb(155,21,25);height: 210px;"><a href="#"><i class="icon ion-social-instagram-outline" style="color: rgb(255,255,255);"></i></a><a href="#"><i class="icon ion-email" style="color: rgb(255,255,255);"></i></a><a href="#"><i class="icon ion-social-facebook" style="color: rgb(255,255,255);"></i></a>
+    <a
+        href="#"><i class="icon ion-social-whatsapp-outline" style="color: rgb(255,255,255);"></i></a>
+        <a href="articadev.com"><p style="margin-top: 3vh;color: rgb(255,255,255);font-size: 1.25em;">Desenvolvido por Artica</p></a>
+</div>
+<!-- End: Footer -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+<script src="assets/js/Simple-Slider.js"></script>
 </body>
 
 </html>
