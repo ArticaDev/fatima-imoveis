1f5243f400197b7693d5091555031392c6f6f218
Lucas Lima <limalucas454@gmail.com>
2020-06-23 16:25:39 -0300

    Added mobile header, margin corrections and stylization.

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,9 +1,19 @@
 $(function(){
-  $("#search").click(function(){
-    $("#searchbar").slideToggle();
-    $(".deskslider").fadeToggle("slow");
+  $(".search").click(function(){
+    $("#searchbar").slideToggle()
+    $(".deskslider").fadeToggle("slow")
     $("#seta").toggleClass("fa fa-arrow-down")
-    $("#seta").toggleClass("fa fa-arrow-up")
-})
+    $("#seta").toggleClass("fa fa-arrow-up")})
 
-})
+    var tela = window.innerWidth
+
+    if (tela>768){
+      $("#nav-desk").show()
+      $("#nav-mob").hide()
+    }
+    else{
+      $("#nav-desk").hide()
+      $("#nav-mob").show()
+    }
+
+  })
