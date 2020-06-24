beb6bd1f0ef02e7c9b5c97d733d3bc26ea30a02c
Lucas Lima <limalucas454@gmail.com>
2020-06-24 17:12:45 -0300

    Added scrolling navbar change, scroll on click and improvements.

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,8 +1,51 @@
 $(function(){
+var tela = window.innerWidth
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > 10) {
+               $(".navbar").addClass("active")
+
+       } else {
+              $(".navbar").removeClass("active")
+       }
+  })
+
+
   $(".search").click(function(){
     $("#searchbar").slideToggle()
-    $(".deskslider").fadeToggle("slow")
-    $("#seta").toggleClass("fa fa-arrow-down")
+    if (tela>1199){
+      $(".deskslider").fadeToggle("slow")} })
+
+  $(".nav .nav-link").on("click", function(){
+     $(".nav").find(".active").removeClass("active")
+     $(this).addClass("active")
+  });
+
+
+
+  $(".home").click(function(){
+    $("#searchbar").slideUp()
+    if (tela>1199){
+      $(".deskslider").fadeIn("slow")}
+    $("html, body").animate({ scrollTop: 0}, 500)
+    $(".nav").find(".active").removeClass("active")
+    $(".home").addClass("active")
+  })
+
+  $("#imoveis").click(function(){
+$("html, body").animate({ scrollTop:0}, 500)
+  })
+
+  $("#contato").click(function(){
+$("html, body").animate({ scrollTop: $(document).height() }, 500)
+  })
+
+})
+
+
+
+
+    /*
+  $("#seta").toggleClass("fa fa-arrow-down")
     $("#seta").toggleClass("fa fa-arrow-up")})
 
     var tela = window.innerWidth
@@ -13,7 +56,4 @@ $(function(){
     }
     else{
       $("#nav-desk").hide()
-      $("#nav-mob").show()
-    }
-
-  })
+      $("#nav-mob").show()  */
